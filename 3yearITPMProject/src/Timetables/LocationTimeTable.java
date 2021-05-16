2509242c04a09be45e07360f83f81dae7866acb2
mahendraetampawala <mahendraetampawala98@gmail.com>
2021-05-17 00:12:52 +0530

    Initial Commit

---
+++ 3yearITPMProject/src/Timetables/LocationTimeTable.java
@@ -0,0 +1,194 @@
+package Timetables;
+
+import java.awt.Color;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.SwingConstants;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JScrollPane;
+
+public class LocationTimeTable {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					LocationTimeTable window = new LocationTimeTable();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public LocationTimeTable() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(30, 30, 1550, 750);
+		frame.getContentPane().setBackground(new Color(51, 0, 0));
+		frame.getContentPane().setLayout(null);
+		
+		JPanel panel = new JPanel();
+		panel.setLayout(null);
+		panel.setBackground(new Color(153, 255, 0));
+		panel.setBounds(10, 10, 1520, 68);
+		frame.getContentPane().add(panel);
+		
+		JLabel lblNewLabel = new JLabel("TIMETABLE MANAGEMENT");
+		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel.setForeground(Color.BLUE);
+		lblNewLabel.setFont(new Font("Times New Roman", Font.BOLD, 22));
+		lblNewLabel.setBackground(Color.BLUE);
+		lblNewLabel.setBounds(430, 0, 329, 68);
+		panel.add(lblNewLabel);
+		
+		JPanel panel_1 = new JPanel();
+		panel_1.setLayout(null);
+		panel_1.setBackground(new Color(51, 51, 102));
+		panel_1.setBounds(10, 88, 233, 615);
+		frame.getContentPane().add(panel_1);
+		
+		JButton btnNewButton = new JButton("Home");
+		btnNewButton.setForeground(Color.BLACK);
+		btnNewButton.setFont(new Font("Times New Roman", Font.BOLD, 20));
+		btnNewButton.setBackground(Color.CYAN);
+		btnNewButton.setBounds(10, 10, 213, 38);
+		panel_1.add(btnNewButton);
+		
+		JButton btnNewButton_1 = new JButton("Lecturers");
+		btnNewButton_1.setFont(new Font("Times New Roman", Font.BOLD, 20));
+		btnNewButton_1.setBackground(Color.CYAN);
+		btnNewButton_1.setBounds(10, 65, 213, 38);
+		panel_1.add(btnNewButton_1);
+		
+		JButton btnNewButton_2 = new JButton("Students");
+		btnNewButton_2.setFont(new Font("Times New Roman", Font.BOLD, 20));
+		btnNewButton_2.setBackground(Color.CYAN);
+		btnNewButton_2.setBounds(10, 124, 213, 38);
+		panel_1.add(btnNewButton_2);
+		
+		JButton btnNewButton_3 = new JButton("Subjects");
+		btnNewButton_3.setFont(new Font("Times New Roman", Font.BOLD, 20));
+		btnNewButton_3.setBackground(Color.CYAN);
+		btnNewButton_3.setBounds(10, 181, 213, 38);
+		panel_1.add(btnNewButton_3);
+		
+		JButton btnNewButton_4 = new JButton("Working days/Hours");
+		btnNewButton_4.setFont(new Font("Times New Roman", Font.BOLD, 20));
+		btnNewButton_4.setBackground(Color.CYAN);
+		btnNewButton_4.setBounds(10, 235, 213, 38);
+		panel_1.add(btnNewButton_4);
+		
+		JButton btnNewButton_5 = new JButton("Sessions");
+		btnNewButton_5.setFont(new Font("Times New Roman", Font.BOLD, 20));
+		btnNewButton_5.setBackground(Color.CYAN);
+		btnNewButton_5.setBounds(10, 289, 213, 38);
+		panel_1.add(btnNewButton_5);
+		
+		JButton btnNewButton_6 = new JButton("Locations");
+		btnNewButton_6.setFont(new Font("Times New Roman", Font.BOLD, 20));
+		btnNewButton_6.setBackground(Color.CYAN);
+		btnNewButton_6.setBounds(10, 344, 213, 38);
+		panel_1.add(btnNewButton_6);
+		
+		JButton btnNewButton_7 = new JButton("Tags");
+		btnNewButton_7.setFont(new Font("Times New Roman", Font.BOLD, 20));
+		btnNewButton_7.setBackground(Color.CYAN);
+		btnNewButton_7.setBounds(10, 400, 213, 38);
+		panel_1.add(btnNewButton_7);
+		
+		JButton btnNewButton_8 = new JButton("Session Rooms");
+		btnNewButton_8.setFont(new Font("Times New Roman", Font.BOLD, 20));
+		btnNewButton_8.setBackground(Color.CYAN);
+		btnNewButton_8.setBounds(10, 456, 213, 38);
+		panel_1.add(btnNewButton_8);
+		
+		JButton btnNewButton_9 = new JButton("Generate Timetables");
+		btnNewButton_9.setFont(new Font("Times New Roman", Font.BOLD, 20));
+		btnNewButton_9.setBackground(Color.CYAN);
+		btnNewButton_9.setBounds(10, 512, 213, 38);
+		panel_1.add(btnNewButton_9);
+		
+		JButton btnNewButton_10 = new JButton("Statistics");
+		btnNewButton_10.setFont(new Font("Times New Roman", Font.BOLD, 20));
+		btnNewButton_10.setBackground(Color.CYAN);
+		btnNewButton_10.setBounds(10, 567, 213, 38);
+		panel_1.add(btnNewButton_10);
+		
+		JPanel panel_2 = new JPanel();
+		panel_2.setLayout(null);
+		panel_2.setBackground(new Color(51, 51, 153));
+		panel_2.setBounds(253, 88, 1277, 76);
+		frame.getContentPane().add(panel_2);
+		
+		JButton btnNewButton_11 = new JButton("Lecturers");
+		btnNewButton_11.setForeground(Color.BLACK);
+		btnNewButton_11.setFont(new Font("Times New Roman", Font.BOLD, 20));
+		btnNewButton_11.setBackground(new Color(204, 255, 255));
+		btnNewButton_11.setBounds(10, 10, 266, 56);
+		panel_2.add(btnNewButton_11);
+		
+		JButton btnNewButton_12 = new JButton("Student");
+		btnNewButton_12.setFont(new Font("Times New Roman", Font.BOLD, 20));
+		btnNewButton_12.setBackground(new Color(204, 255, 255));
+		btnNewButton_12.setBounds(1001, 9, 266, 58);
+		panel_2.add(btnNewButton_12);
+		
+		JButton btnNewButton_12_1 = new JButton("Location");
+		btnNewButton_12_1.setFont(new Font("Times New Roman", Font.BOLD, 20));
+		btnNewButton_12_1.setBackground(new Color(204, 255, 255));
+		btnNewButton_12_1.setBounds(499, 9, 266, 58);
+		panel_2.add(btnNewButton_12_1);
+		
+		JPanel panel_3 = new JPanel();
+		panel_3.setLayout(null);
+		panel_3.setBackground(new Color(204, 255, 102));
+		panel_3.setBounds(253, 177, 1277, 526);
+		frame.getContentPane().add(panel_3);
+		
+		JButton btnNewButton_14 = new JButton("Print");
+		btnNewButton_14.setFont(new Font("Times New Roman", Font.BOLD, 19));
+		btnNewButton_14.setBackground(Color.CYAN);
+		btnNewButton_14.setBounds(1039, 30, 228, 40);
+		panel_3.add(btnNewButton_14);
+		
+		JButton btnNewButton_15 = new JButton("Generate");
+		btnNewButton_15.setFont(new Font("Times New Roman", Font.BOLD, 19));
+		btnNewButton_15.setBackground(Color.CYAN);
+		btnNewButton_15.setBounds(771, 30, 228, 40);
+		panel_3.add(btnNewButton_15);
+		
+		JComboBox comboBox = new JComboBox();
+		comboBox.setFont(new Font("Times New Roman", Font.BOLD, 20));
+		comboBox.setBackground(Color.WHITE);
+		comboBox.setBounds(318, 31, 339, 39);
+		panel_3.add(comboBox);
+		
+		JLabel lblNewLabel_1 = new JLabel("Lecturer Name");
+		lblNewLabel_1.setFont(new Font("Times New Roman", Font.BOLD, 18));
+		lblNewLabel_1.setBounds(176, 30, 132, 40);
+		panel_3.add(lblNewLabel_1);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
